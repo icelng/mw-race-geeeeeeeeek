3758d19eeca2b4f501aec35a7638a71bbcd4e622
yiran_fancier <ip28@qq.com>
2018-07-22 02:09:11 +0800

    方案大改,异步,10个线程

--- src/main/java/com/alibaba/mwrace2018/geeks/FlushService.java
+++ src/main/java/com/alibaba/mwrace2018/geeks/FlushService.java
@@ -15,41 +15,48 @@ import java.util.concurrent.Semaphore;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 public class FlushService {
+    private static final int THREAD_NUM = 10;
     private static final TraceLogComparator COMPARATOR = new TraceLogComparator();
     private Semaphore requestNum = new Semaphore(0);
     private Deque<TraceLogList> requestQueue = new ConcurrentLinkedDeque<>();
     private String outputDir;
-    private Thread thread;
+    private Thread thread[] = new Thread[THREAD_NUM];
     private AtomicBoolean stopFlag = new AtomicBoolean(false);
 
     public FlushService(String outputDir) {
         this.outputDir = outputDir;
-        thread = new Thread(() -> {
 
-            while (!stopFlag.get()) {
-                try {
-                    requestNum.acquire();
-                } catch (InterruptedException e) {
-                    break;
+        for (int i = 0;i < THREAD_NUM;i++) {
+            thread[i] = new Thread(() -> {
+
+                while (!stopFlag.get()) {
+                    try {
+                        requestNum.acquire();
+                    } catch (InterruptedException e) {
+                        break;
+                    }
+                    TraceLogList traceLogList = requestQueue.pollFirst();
+                    String filePath = this.outputDir + "/" + traceLogList.getTraceLogs().get(0).getTraceId();
+                    CharSink sink = Files.asCharSink(new File(filePath), Charset.forName("UTF-8"));
+                    try {
+                        sink.writeLines(this.sort(traceLogList.getTraceLogs()));
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
                 }
-                TraceLogList traceLogList = requestQueue.pollFirst();
-                String filePath = this.outputDir + "/" + traceLogList.getTraceLogs().get(0).getTraceId();
-                CharSink sink = Files.asCharSink(new File(filePath), Charset.forName("UTF-8"));
-                try {
-                    sink.writeLines(this.sort(traceLogList.getTraceLogs()));
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
 
-            }
+            });
+            thread[i].start();
 
-        });
-        thread.start();
+        }
     }
 
     public void stop(){
         stopFlag.set(true);
-        thread.interrupt();
+        for (int i = 0;i < THREAD_NUM;i++) {
+            thread[i].interrupt();
+        }
 //        requestNum.notify();
     }
 
