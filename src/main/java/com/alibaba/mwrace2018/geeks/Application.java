1dd4e410b20ba011670c0627cd0e258629228a21
yiran_fancier <ip28@qq.com>
2018-07-21 17:34:34 +0800

    页大小为2k

--- src/main/java/com/alibaba/mwrace2018/geeks/Application.java
+++ src/main/java/com/alibaba/mwrace2018/geeks/Application.java
@@ -62,8 +62,8 @@ public class Application implements CommandLineRunner {
         LOGGER.info("dataUrl = {}", this.dataUrl);
 
         storeIO = new StoreIO(outputDir + "/log", FILE_SIZE, REGION_SIZE);
-        idlePageManager = new SingleUseIdlePageManager(FILE_SIZE, 4096);
-        residentPageCachePool = new ResidentPageCachePool(65535, 4096);
+        idlePageManager = new SingleUseIdlePageManager(FILE_SIZE, 2048);
+        residentPageCachePool = new ResidentPageCachePool(65535, 2048);
 
         CharSource source = Resources.asCharSource(new URL(this.dataUrl), Charset.forName("UTF-8"));
 //        URL url = Resources.getResource(this.dataUrl);
